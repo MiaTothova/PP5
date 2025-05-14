94a233e00e5f97e2d9fd37ef2f015013fe69cedc
MiaTothova <21tothova@gmail.com>
2025-05-14 14:14:16 +0100

    Added adjust bag and remove from bag view, calc subtotal filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
+from django.urls import reverse
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -22,3 +25,34 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+            bag[item_id] = quantity
+    else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove an item from the shopping bag (no size handling) """
+    try:
+        bag = request.session.get('bag', {})
+
+        if item_id in bag:
+            bag.pop(item_id)
+            request.session['bag'] = bag
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=404)  
+
+    except Exception as e:
+        return HttpResponse(status=500)
