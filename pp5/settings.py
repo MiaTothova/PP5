3f84a5a4040dddb1279241cbae7a4ee9dd2f8ae7
MiaTothova <21tothova@gmail.com>
2025-05-20 14:34:49 +0100

    collect static files for deployment

--- pp5/settings.py
+++ pp5/settings.py
@@ -57,13 +57,14 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
 
-      # Other
+    # Other
     'crispy_forms',
 
     ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -180,6 +181,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
