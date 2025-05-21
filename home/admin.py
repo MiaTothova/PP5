c8debc30fc4c42fcf1e89d882258c7cf0efe9553
MiaTothova <21tothova@gmail.com>
2025-05-21 12:04:15 +0100

    registered contact for in admin page

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import NewsletterSubscription
+from .models import NewsletterSubscription, ContactMessage
 
 @admin.register(NewsletterSubscription)
 class NewsletterAdmin(admin.ModelAdmin):
     list_display = ('email', 'subscribed_on')
+    
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'sent_on')  
+    readonly_fields = ('sent_on',)
