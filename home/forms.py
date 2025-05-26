a142b325e2fcef8916b04cd48f950c80457f4870
MiaTothova <21tothova@gmail.com>
2025-05-26 03:52:13 +0100

    Change back to recent code

--- home/forms.py
+++ home/forms.py
@@ -1,18 +1,11 @@
 from django import forms
 from .models import NewsletterSubscription, ContactMessage
-from crispy_forms.helper import FormHelper
 
 
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model = NewsletterSubscription
         fields = ['email']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id = 'newsletter-form'
-        self.helper.auto_id = False
 
 
 class ContactForm(forms.ModelForm):
