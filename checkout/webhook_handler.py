cbb6ecbdb95ac028e1b14e8c2cbc6b479b218e41
MiaTothova <21tothova@gmail.com>
2025-08-05 22:01:26 +0100

    Fix type error in webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -20,8 +20,7 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
-        print(">>> SENDING EMAIL NOW <<<")
-        cust_email = order.email
+        order_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
