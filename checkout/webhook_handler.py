b72988e9678ba1f3f0bfa154f3f505da4d58d262
MiaTothova <21tothova@gmail.com>
2025-08-05 21:46:45 +0100

    Change a mistake in the webhook handler re- order.email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -33,7 +33,7 @@ class StripeWH_Handler:
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
+            [order.email]
         )
 
     def handle_event(self, event):
